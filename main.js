90f47ef2c2fce82c8c17c95c57d1acfffdab8dcb
Shance Lin <shancelin@Shances-MacBook.local>
2021-04-28 11:07:59 -0400

    Created main.js with array of integer phonetics, also saving command line args

---
+++ main.js
@@ -0,0 +1,6 @@
+//Save the command line arguments save the first 2 (node & filename)
+var args = process.argv.slice(2);
+
+//array of words of numbers;
+var nums = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+
